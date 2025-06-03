bc454b5bb6f6f089bf5ef9ab70cfe9d7845881e2
shawnrmelton <shawnryanmelton@gmail.com>
2025-06-03 15:14:46 -0400

    Update projects.js

---
+++ task-flow-manager/backend/src/routes/projects.js
@@ -0,0 +1,11 @@
+// backend/src/routes/projects.js
+const express = require('express');
+const router = express.Router();
+const projectController = require('../controllers/projectController');
+
+router.get('/', projectController.getProjects);
+router.post('/', projectController.createProject);
+router.put('/:id', projectController.updateProject);
+router.delete('/:id', projectController.deleteProject);
+
+module.exports = router;
