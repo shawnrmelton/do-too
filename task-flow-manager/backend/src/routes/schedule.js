0c8d31cdfb4cf1b3841934a159f6bc8227b0f458
shawnrmelton <shawnryanmelton@gmail.com>
2025-06-03 21:31:15 -0400

    Delete task-flow-manager directory

--- task-flow-manager/backend/src/routes/schedule.js
+++
@@ -1,8 +0,0 @@
-// backend/src/routes/schedule.js
-const express = require('express');
-const router = express.Router();
-const scheduleController = require('../controllers/scheduleController');
-
-router.get('/generate', scheduleController.generateSchedule);
-
-module.exports = router;
